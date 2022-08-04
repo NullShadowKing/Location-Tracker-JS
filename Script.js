61eaebfe5a05f10f644457eb383ad06230620172
iman mohammadi <Imanking251@gmail.com>
2022-08-04 22:30:16 +0430

    Some parts of code optimised

--- Script.js
+++ Script.js
@@ -11,37 +11,56 @@ const btnEl = document.querySelector(".btn");
 let ipdata = "";
 let countrydata = "";
 
-const getData = function () {
-  fetch("http://ip-api.com/json/")
-    .then((Response) => {
-      if (!Response.ok) throw new Error(`${errorMsg} ${Response.status}`);
-      return Response.json();
-    })
-    .then((data) => {
-      ipdata = data;
-      fetch(`https://restcountries.com/v3.1/alpha/${ipdata.countryCode}`)
-        .then((Response) => {
-          if (!Response.ok) throw new Error(`${errorMsg} ${Response.status}`);
-          return Response.json();
-        })
-        .then((data) => {
-          countrydata = data;
-          const [geodata] = countrydata;
-          console.log();
-          const countryCity = `${ipdata.country.toUpperCase()}, ${ipdata.city}`;
-          const coords = `Coordinate: ${ipdata.lat}, ${ipdata.lon}`;
-          const region = `Region: ${geodata.region}`;
-          const currency = `Currency: ${
-            geodata.currencies[Object.keys(geodata.currencies)].name
-          }`;
-          const ipaddress = `IP Address: ${ipdata.query}`;
-          const flagaddress = geodata.flags[Object.keys(geodata.flags)[0]];
-          init(countryCity, coords, region, currency, ipaddress, flagaddress);
-          btnEl.style.opacity = 1;
-        });
-    })
-    .catch((err) => console.log(`Something Went Wrong, ${err.message}`));
+const correctFormmating = function (datap, datac) {
+  const [geodata] = datac;
+  const countryCity = `${datap.country.toUpperCase()}, ${datap.city}`;
+  const coords = `Coordinate: ${datap.lat}, ${datap.lon}`;
+  const region = `Region: ${geodata.region}`;
+  const currency = `Currency: ${
+    geodata.currencies[Object.keys(geodata.currencies)].name
+  }`;
+  const ipaddress = `IP Address: ${datap.query}`;
+  const flagaddress = geodata.flags[Object.keys(geodata.flags)[0]];
+  init(countryCity, coords, region, currency, ipaddress, flagaddress);
+  btnEl.style.opacity = 1;
 };
+
+const newGetData = async function () {
+  const response = await fetch("http://ip-api.com/json/");
+  if (!response.ok) throw new Error(`${errorMsg} ${Response.status}`);
+  const data = await response.json();
+  ipdata = data;
+  const countryres = await fetch(
+    `https://restcountries.com/v3.1/alpha/${ipdata.countryCode}`
+  );
+  if (!countryres.ok) throw new Error(`${errorMsg} ${countryres.status}`);
+  const dataC = await countryres.json();
+  countrydata = dataC;
+  correctFormmating(ipdata, countrydata);
+};
+
+// const getData = function () {
+//   fetch("http://ip-api.com/json/")
+//     .then((Response) => {
+//       if (!Response.ok) throw new Error(`${errorMsg} ${Response.status}`);
+//       return Response.json();
+//     })
+//     .then((data) => {
+//       ipdata = data;
+//       fetch(`https://restcountries.com/v3.1/alpha/${ipdata.countryCode}`)
+//         .then((Response) => {
+//           if (!Response.ok) throw new Error(`${errorMsg} ${Response.status}`);
+//           return Response.json();
+//         })
+//         .then((data) => {
+//           countrydata = data;
+//           correctFormmating(ipdata, countrydata);
+//         });
+//     })
+//     .catch((err) => console.log(`Something Went Wrong, ${err.message}`));
+// };
+// getData();
+
 const init = function (
   country,
   coords,
@@ -58,7 +77,7 @@ const init = function (
   flagEl.src = imgaddress;
 };
 
-getData();
+newGetData();
 
 btnEl.addEventListener("click", function () {
   window.open(
